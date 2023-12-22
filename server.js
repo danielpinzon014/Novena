69b6ec1072a2e949e95f174112b803b4925f7114
Daniel Pinzón <48761874+danielpinzon014@users.noreply.github.com>
2023-12-22 15:28:27 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,18 +6,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const votes = Array(3).fill(0); // Inicializar un array de votos con la longitud de tu conjunto de cards
+let votes = Array(3).fill(0);
 
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   console.log('Nuevo usuario conectado');
 
+  // Enviar votos actuales al nuevo usuario
+  socket.emit('updateVotes', votes);
+
   socket.on('vote', (index) => {
-    // Incrementar el voto en la posición correspondiente del array
     if (index >= 0 && index < votes.length) {
       votes[index]++;
-      // Emitir a todos los clientes para actualizar los votos
       io.emit('updateVotes', votes);
     }
   });
